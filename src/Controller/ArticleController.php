a59e023b8e79fe238cf18dbca63771518f39a5ea
dmytri-bykovski <dmitry.bykovski.work@gmail.com>
2021-04-04 17:05:09 +0300

    Twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,7 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+
+class ArticleController extends AbstractController
 {
 	/**
 	 * @Route("/")
@@ -21,6 +22,15 @@ class ArticleController
 	 */
 	public function show($slug)
 	{
-		return new Response(sprintf('Mother Fucker <br> %s',$slug));
+		$comments = [
+		"I ate a normal rock once. It did NOT taste like bacon!",
+		"Woohoo! I'm going on an all-asteroid diet!",
+		"I like bacon too! Buy some from my site! bakinsomebacon.com",
+	];
+
+		return $this->render('article/show.html.twig', [
+			'title' => ucwords(str_replace('-', ' ', $slug)),
+			'comments' => $comments,
+		]);
 	}
 }
