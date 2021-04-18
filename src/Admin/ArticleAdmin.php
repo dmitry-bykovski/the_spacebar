51ff454b03ee151829ddccfecbbb20b4769b39d9
dmytri-bykovski <dmitry.bykovski.work@gmail.com>
2021-04-18 21:02:46 +0300

    2

--- src/Admin/ArticleAdmin.php
+++ src/Admin/ArticleAdmin.php
@@ -7,31 +7,33 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use function Sodium\add;
 
 final class ArticleAdmin extends AbstractAdmin
 {
 	protected function configureFormFields(FormMapper $formMapper): void
 	{
 //		$formMapper->add('id', TextType::class);
-		$formMapper->add('title', TextType::class);
-		$formMapper->add('slug', TextType::class);
-		$formMapper->add('content', TextType::class);
-//		$formMapper->add('publishedAt', TextType::class);
-		$formMapper->add('author', TextType::class);
-		$formMapper->add('heart_count', TextType::class);
-		$formMapper->add('image_filename', TextType::class);
-//		$formMapper->add('createdAt', TextType::class);
-//		$formMapper->add('updateAt', TextType::class);
+		$formMapper->add('title', TextType::class)
+			->add('slug', TextType::class)
+			->add('content', TextType::class)
+			->add('author', TextType::class)
+			->add('heart_count', TextType::class)
+			->add('image_filename', TextType::class);
+//			->add('createdAt', TextType::class)
+//			->add('updateAt', TextType::class);
+
+
 	}
 
 	protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
 	{
-		$datagridMapper->add('id');
-		$datagridMapper->add('title');
-		$datagridMapper->add('content');
-		$datagridMapper->add('slug');
-		$datagridMapper->add('publishedAt');
-		$datagridMapper->add('author');
+		$datagridMapper->add('id')
+			->add('title')
+			->add('content')
+			->add('slug')
+			->add('publishedAt')
+			->add('author');
 //		$datagridMapper->add('heart_count');
 //		$datagridMapper->add('image_filename');
 //		$datagridMapper->add('createdAt');
@@ -40,15 +42,15 @@ final class ArticleAdmin extends AbstractAdmin
 
 	protected function configureListFields(ListMapper $listMapper): void
 	{
-		$listMapper->addIdentifier('id');
-		$listMapper->addIdentifier('title');
-		$listMapper->addIdentifier('slug');
-		$listMapper->addIdentifier('content');
-		$listMapper->addIdentifier('publishedAt');
-		$listMapper->addIdentifier('author');
-		$listMapper->addIdentifier('heart_count');
-		$listMapper->addIdentifier('image_filename');
-		$listMapper->addIdentifier('createdAt');
-		$listMapper->addIdentifier('updateAt');
+		$listMapper->addIdentifier('id')
+		->addIdentifier('title')
+		->addIdentifier('slug')
+//		->addIdentifier('content')
+		->addIdentifier('publishedAt')
+		->addIdentifier('author')
+		->addIdentifier('heart_count')
+		->addIdentifier('image_filename')
+		->addIdentifier('createdAt')
+		->addIdentifier('updateAt');
 	}
 }
